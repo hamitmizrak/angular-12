de5ae1a33f315b993e74bd5d24f5240416a0dbb1
HamitMizrak <hamitmizrak@gmail.com>
2021-06-02 17:47:00 +0300

    diziler

---
+++ type-script/typescript-003-datatype.ts
@@ -0,0 +1,22 @@
+//var let const
+// DATA TYPE-2
+
+//unknown
+let sayi:unknown=44;
+sayi="Malatya";
+sayi=true;
+sayi=44.44;
+
+
+//diziler
+var deneme=[];
+let dizi:string[]=["Malatya","Darende","Battalgazi","Yeşilyurt"]
+let dizi2:number[]=[4,6,9];
+
+//diziler Generics  Array<Customer>
+const dizi3:Array<string>=["Hamit"];
+const dizi4:Array<number>=[55];
+
+//tuple=demet
+let tupleDizi:[number,string,boolean,number]=[44,"Merhabalar",true,55]
+
