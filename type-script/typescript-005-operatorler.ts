af725c3dc26475053719cca06c669809488680c7
HamitMizrak <hamitmizrak@gmail.com>
2021-06-02 23:29:34 +0300

    operatorler

---
+++ type-script/typescript-005-operatorler.ts
@@ -0,0 +1,17 @@
+//operatorler
+let sayi1:number=10;
+let sayi2:number=20;
+let sayi3:string="20";
+let result2:number=0;
+
+
+result2=sayi1+sayi2;
+result2=sayi1-sayi2;
+result2=sayi1*sayi2;
+result2=sayi1/sayi2;
+result2=sayi1%sayi2;
+
+///sayi2==sayi3 =true
+//sayi===sayi3 =false
+//sayi2&&sayi3 ||sayi1
+//!sayi2
