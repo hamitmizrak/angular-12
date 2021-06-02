b05ef2bbe641dfbe453629e5fa610878437ccfa8
HamitMizrak <hamitmizrak@gmail.com>
2021-06-02 17:30:25 +0300

    datatypes

--- type-script/typescript-001-tsTojs.js
+++ type-script/typescript-001-tsTojs.js
@@ -1,5 +1,5 @@
 function tsTojs(isim, soyisim) {
     return isim + " " + soyisim;
 }
-var fullName = tsTojs("Hamit", 'Mızrak');
+var fullName = tsTojs("Hamit", 'Mızrak 44 asdsad');
 console.log(fullName);
---
+++ type-script/typescript-002-datatype.ts
@@ -0,0 +1,21 @@
+//var let const
+// DATA TYPE
+//////////////  number //////////////////////
+//2lik tabanda
+let binary:number=0b0110;
+
+//8lik tabanda
+let octal:number=0o777;
+
+//10lık tabanda
+let decimal:number=50845;
+
+//16 tabanda
+let hexadecimal:number=0xfff;
+
+////////////////// boolean /////////////////////////////////
+let result:boolean=true;
+
+///////////////// string //////////////////////////////////
+const kelime:string="Merhabalar nasılsınız";
+
