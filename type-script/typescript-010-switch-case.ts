1f75a99130483ef6ea9e11bc1f1997e9a3e040c6
HamitMizrak <hamitmizrak@gmail.com>
2021-06-03 00:15:37 +0300

    switch-case

---
+++ type-script/typescript-010-switch-case.ts
@@ -0,0 +1,32 @@
+function gunler7(gunler6:number):void {
+
+switch (gunler6) {
+    case 1:
+        console.log("pazartesi")
+        break;
+        case 1:
+            console.log("salı")
+            break;
+            case 1:
+        console.log("çarşamba")
+        break;
+        case 1:
+        console.log("perşembe")
+        break;
+        case 1:
+        console.log("cuma")
+        break;
+        case 1:
+        console.log("cumartesi")
+        break;
+        case 1:
+        console.log("pazar")
+        break;
+    default:
+        console.log("lütfen belirtilen aralıkta sayı giriniz")
+        break;
+}
+
+
+
+gunler7(4);
