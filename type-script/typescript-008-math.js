0d029410a48d0f1f7e10c26a8695dc920ab318c6
HamitMizrak <hamitmizrak@gmail.com>
2021-06-03 00:02:20 +0300

    math function

---
+++ type-script/typescript-008-math.js
@@ -0,0 +1,12 @@
+function mathExamples(sayi) {
+    console.log(Math.sqrt(sayi));
+    console.log(Math.abs(sayi));
+    console.log(Math.pow(2, sayi));
+    console.log(Math.round(sayi));
+    console.log(Math.ceil(sayi));
+    console.log(Math.floor(sayi));
+    console.log(Math.min(sayi, sayi * 2, sayi * -1));
+    console.log(Math.max(sayi, sayi * 2, sayi * -1));
+    console.log(Math.random() * 2 + 1);
+}
+mathExamples(16);
