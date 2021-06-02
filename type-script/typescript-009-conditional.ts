9c6aa53f6abc7a406a5ba9f844914fd0c7070b5a
HamitMizrak <hamitmizrak@gmail.com>
2021-06-03 00:11:27 +0300

    condiitonal

---
+++ type-script/typescript-009-conditional.ts
@@ -0,0 +1,22 @@
+function gunler(gunler6:number):void {
+if(gunler6==1){
+    console.log("pazartesi")
+}else if(gunler6==2){
+    console.log("sali")
+}else if(gunler6==3){
+    console.log("çarşamba")
+}else if(gunler6==4){
+    console.log("perşembe")
+}else if(gunler6==5){
+    console.log("cuma")
+}else if(gunler6==6){
+    console.log("cumartesi")
+}else if(gunler6==7){
+    console.log("pazar")
+}else{
+    console.log("lütfen belirtilen aralıkta sayı giriniz")
+}
+
+}
+
+gunler(4);
