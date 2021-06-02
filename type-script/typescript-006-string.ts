ad1c9cd509f0478c567ea40333f233763cbc4808
HamitMizrak <hamitmizrak@gmail.com>
2021-06-02 23:41:41 +0300

    string method

---
+++ type-script/typescript-006-string.ts
@@ -0,0 +1,15 @@
+//string
+let kelime5:string="Servlet Jsp JSF Spring Boot hibernate rest ";
+console.log(kelime5.length);
+console.log(kelime5.trim.length);
+console.log(kelime5.replace("Servlet","Java se"));
+console.log(kelime5.concat(" sona ekledim"));
+console.log("substring"+kelime5.substring(0,10));
+console.log(kelime5.toLowerCase);
+console.log(kelime5.toLowerCase);
+console.log(kelime5.lastIndexOf("rest"));
+console.log(kelime5.indexOf("Servlet"));
+console.log(kelime5.startsWith("S"));
+console.log(kelime5.endsWith(" "));
+//console.log(kelime5.);
+//console.log(kelime5.);
