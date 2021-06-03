0d35d318d3939f69d02fe6569b2952e9a35beeca
HamitMizrak <hamitmizrak@gmail.com>
2021-06-03 23:42:02 +0300

    return void

---
+++ type-script/typescript-016-return-void.ts
@@ -0,0 +1,18 @@
+//voidli
+function donussuz():void{
+    console.log("voidli")
+}
+
+function donussuz2(adi:string,yas:number):void{
+    console.log("voidli"+adi+Number)
+}
+
+//voidsiz
+  
+function donuslu():string{
+    return "voidsiz parametresiz";
+}
+
+function donuslu2(adi:string,yas:number):string{
+    return adi+" voidsiz parametreli "+yas;
+}
