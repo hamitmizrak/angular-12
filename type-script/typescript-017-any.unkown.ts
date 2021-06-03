693347a48b0dec3df21d9ed5e5baf94636107df6
HamitMizrak <hamitmizrak@gmail.com>
2021-06-03 23:42:16 +0300

    any unknown

---
+++ type-script/typescript-017-any.unkown.ts
@@ -0,0 +1,11 @@
+//unknown
+//servis db türünün ne olduğunu bilmediğim zamanlarda bunları kullanıyoruz.
+let kelime17:unknown=10;
+kelime17=44.52;
+kelime17="44.52;"
+
+//any
+//servis db türünün ne olduğunu bilmediğim zamanlarda bunları kullanıyoruz.
+let kelime18:any=10;
+kelime18=44.52;
+kelime18="44.52;"
