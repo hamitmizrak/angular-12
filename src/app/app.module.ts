c977c6644dc2017f31f1aa95eaab0adcd394c0f4
HamitMizrak <hamitmizrak@gmail.com>
2021-06-05 22:57:13 +0300

    font-awesome eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// componetler  import
+// componentler  import
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 
-// componetler  use
+// componentler  use
 @NgModule({
   declarations: [
     AppComponent,
