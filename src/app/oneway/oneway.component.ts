0e7e2e81b019213955773639788f5b8d59e9a724
HamitMizrak <hamitmizrak@gmail.com>
2021-06-07 17:19:30 +0300

    2-)property binding

--- src/app/oneway/oneway.component.ts
+++ src/app/oneway/oneway.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let alertify:any;
 
 @Component({
   selector: 'app-oneway',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnewayComponent implements OnInit {
 
-  adi="Java EE";
+  //1-)string interpolation
+  adi:string="string interpolation binding";
+
+  //2-)property binding"
+  htmlContent:string="property binding";
+
+  
 
   constructor() { }
 
